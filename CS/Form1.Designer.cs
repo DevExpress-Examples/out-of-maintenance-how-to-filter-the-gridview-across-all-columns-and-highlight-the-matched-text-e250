2608b79ee7afe0694cd465dde9a29a6b8b5a1c95
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿// Developer Express Code Central Example:
+// How to filter the GridView across all columns and highlight the matched text
+// 
+// This example demonstrates how to show only those rows in the GridView whose
+// cells contain a specified text. Matched text is highlighted using the
+// MultiColorDrawString method.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2508
+
+namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(515, 480);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1,
+            this.gridView2});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // gridView2
+            // 
+            this.gridView2.GridControl = this.gridControl1;
+            this.gridView2.Name = "gridView2";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEdit1.Location = new System.Drawing.Point(267, 12);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.NullValuePrompt = "Type your filter here...";
+            this.textEdit1.Properties.NullValuePromptShowForEmptyValue = true;
+            this.textEdit1.Size = new System.Drawing.Size(219, 20);
+            this.textEdit1.TabIndex = 1;
+            this.textEdit1.EditValueChanged += new System.EventHandler(this.textEdit1_EditValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 480);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+    }
+}
+
---
+++ CS/GridViewFilter.cs
@@ -0,0 +1,89 @@
+﻿// Developer Express Code Central Example:
+// How to filter the GridView across all columns and highlight the matched text
+// 
+// This example demonstrates how to show only those rows in the GridView whose
+// cells contain a specified text. Matched text is highlighted using the
+// MultiColorDrawString method.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2508
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.Data.Filtering;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Utils.Paint;
+using DevExpress.Utils;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraEditors.ViewInfo;
+using DevExpress.LookAndFeel;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+
+namespace WindowsApplication1
+{
+    public class GridViewFilterHelper
+    {
+        private string _ActiveFilter = string.Empty;
+        private GridView _View;
+        XPaint paint = new XPaint();
+       
+        public string ActiveFilter
+        {
+            get { return _ActiveFilter; }
+            set
+            {
+                if (_ActiveFilter != value)
+                    _ActiveFilter = value;
+                OnActiveFilterChanged();
+            }
+        }
+
+        public GridViewFilterHelper(GridView view)
+        {
+            _View = view;
+            _View.CustomDrawCell += new RowCellCustomDrawEventHandler(_View_CustomDrawCell);
+        }
+
+        void _View_CustomDrawCell(object sender, RowCellCustomDrawEventArgs e)
+        {
+            if (_ActiveFilter == string.Empty)
+                return;
+            int index = e.DisplayText.IndexOf(_ActiveFilter);
+            if (index < 0)
+                return;
+            e.Handled = true;
+            var inf = (e.Cell as GridCellInfo);
+            TextEditViewInfo vi = inf.ViewInfo as TextEditViewInfo;
+            e.Appearance.FillRectangle(e.Cache, e.Bounds);
+            MultiColorDrawStringParams args = new MultiColorDrawStringParams(e.Appearance);
+            args.Bounds = e.Bounds;
+            args.Text = e.DisplayText;
+            args.Appearance.Assign(e.Appearance);
+            AppearanceDefault apperance = LookAndFeelHelper.GetHighlightSearchAppearance(vi.LookAndFeel, !vi.UseHighlightSearchAppearance);
+            e.Cache.Paint.DrawMultiColorString(e.Cache, e.Bounds, e.DisplayText, _ActiveFilter, vi.PaintAppearance, vi.PaintAppearance.GetTextOptions().GetStringFormat(vi.DefaultTextOptions),
+                apperance.ForeColor, apperance.BackColor, false, index);
+        }
+
+        CriteriaOperator CreateFilterCriteria()
+        {
+            CriteriaOperator[] operators = new CriteriaOperator[_View.VisibleColumns.Count];
+            for (int i = 0; i < _View.VisibleColumns.Count; i++)
+            {
+                operators[i] = new BinaryOperator(_View.VisibleColumns[i].FieldName, String.Format("%{0}%", _ActiveFilter), BinaryOperatorType.Like);
+            }
+            return new GroupOperator(GroupOperatorType.Or, operators);
+
+        }
+
+        private void OnActiveFilterChanged()
+        {
+            _View.ActiveFilterCriteria = CreateFilterCriteria();
+        }
+    }
+}
